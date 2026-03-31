39fb3df4369961feee8c3eaa3cde7fe424ce5022
mensanawellness <mensanawellness@gmail.com>
2026-03-31 12:31:07 -0700

    Change minPayoutCents value in emptyForm
    Updated minPayoutCents from '100000' to '100' in emptyForm.

--- src/app/admin/programs/page.tsx
+++ src/app/admin/programs/page.tsx
@@ -45,7 +45,7 @@ interface Program {
 
 const emptyForm = {
   name: '', slug: '', description: '', commissionRate: '20', commissionType: 'PERCENTAGE',
-  cookieDuration: '30', currency: 'INR', autoApprove: false, minPayoutCents: '100000',
+  cookieDuration: '30', currency: 'INR', autoApprove: false, minPayoutCents: '100',
   payoutFrequency: 'MONTHLY', termsUrl: '', logoUrl: '', brandColor: '#6366f1',
 };
 
